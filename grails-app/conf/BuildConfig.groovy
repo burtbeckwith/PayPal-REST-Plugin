5e2699286920523ac95746ef6237e199a0fb84cb
Burt Beckwith <burt@burtbeckwith.com>
2014-02-09 14:50:34 -0500

    added release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,5 +16,9 @@ grails.project.dependency.resolution = {
 
 	plugins {
 		compile ":jersey-request-builder:1.1.3"
+
+		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+			export = false
+		}
 	}
 }
